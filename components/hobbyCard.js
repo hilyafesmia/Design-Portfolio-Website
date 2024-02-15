9c17207e652b856d994b15b31064a0d42b88f7aa
hilyafesmia <hilyafesmia@gmail.com>
2024-02-15 16:01:55 +0700

    fix hobbycard image property

--- components/hobbyCard.js
+++ components/hobbyCard.js
@@ -1,22 +1,15 @@
-import Link from 'next/link';
-import Image from 'next/image'
+import Link from "next/link";
+import Image from "next/image";
 
-export default function HobbyCard({slug, img, title, type}) {
-    return (
-        <Link href={`/hobby/${slug}`}>
-            <div className="card-item">
-                <div className="animate-border hobby-border">
-                    <Image 
-                        width={1600}
-                        height={900}
-                        alt={title}
-                        src={`/${img}`}
-                    />
-                </div>
-                <hr/>
-                <p>{type}</p>
-                <h2>{title}</h2>
-            </div>
-          </Link>
-    )
+export default function HobbyCard({ slug, img, title, type }) {
+  return (
+    <Link className="card-item" href={`/hobby/${slug}`}>
+      <div className="animate-border hobby-border">
+        <Image fill alt={title} src={`/${img}`} />
+      </div>
+      <hr />
+      <p>{type}</p>
+      <h2>{title}</h2>
+    </Link>
+  );
 }
