e4244f042e48493d4cdeaf78d2e5abad961201b6
hilyafesmia <hilyafesmia@gmail.com>
2024-02-15 16:02:12 +0700

    fix workcard image property

--- components/workCard.js
+++ components/workCard.js
@@ -3,18 +3,16 @@ import Image from "next/image";
 
 export default function WorkCard({ slug, img, duration, title, scope }) {
   return (
-    <Link href={`/work/${slug}`}>
-      <div className="card-item">
-        <div className="animate-border">
-          <Image width={1600} height={900} alt={title} src={`/${img}`} />
-        </div>
-        <hr />
-        <div className="scope-duration">
-          <p>{scope}</p>
-          <p>{duration}</p>
-        </div>
-        <h2>{title}</h2>
+    <Link className="card-item" href={`/work/${slug}`}>
+      <div className="animate-border">
+        <Image fill alt={title} src={`/${img}`} />
       </div>
+      <hr />
+      <div className="scope-duration">
+        <p>{scope}</p>
+        <p>{duration}</p>
+      </div>
+      <h2>{title}</h2>
     </Link>
   );
 }
